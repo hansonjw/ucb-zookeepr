14080c7032c7b7a6aa536a6b16d0facf9bc64982
hansonjw <hansonjw@gmail.com>
2020-09-28 20:36:40 -0500

    Add heroku, new port

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// deployed site on heroku: https://pacific-island-81874.herokuapp.com/api/animals
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -42,6 +44,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query){
@@ -49,6 +56,15 @@ app.get('/api/animals', (req, res) => {
     }
     console.log(req.query);
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
